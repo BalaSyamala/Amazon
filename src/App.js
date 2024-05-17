8911c8225742554b33ce97e6b7795fb327ede44c
BalaSyamala <llakshmi373@gmail.com>
2024-05-17 16:40:45 -0400

    amazon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+const card = [
+  {
+    img: "https://m.media-amazon.com/images/I/41vhe0X8wbL.AC_SX500.jpg",
+    url: "https://www.amazon.com/all-new-amazon-fire-tv-stick-4k/dp/B0BP9MDCQZ/ref=s9_acsd_al_ot_c2_x_0_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=2c28288b-c275-4b38-8798-84c6244351a2&pf_rd_t=&pf_rd_i=8521791011",
 
+    tittle:
+      "All-new Amazon Fire TV Stick 4K streaming device, more than 1.5 million movies and TV episodes",
+    sprice: "$29.99",
+    cprice: "$49.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41P0qtzxWSL.AC_SX500.jpg",
+    url: "https://www.amazon.com/all-new-amazon-fire-tv-stick-4k-max/dp/B0BP9SNVH9/ref=s9_acsd_al_ot_c2_x_1_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=2c28288b-c275-4b38-8798-84c6244351a2&pf_rd_t=&pf_rd_i=8521791011",
+    tittle: "All-new Amazon Fire TV Stick 4K Max streaming device",
+    sprice: "$39.99",
+    cprice: "$59.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41DTzx56EWL.AC_SX500.jpg",
+    url: "https://www.amazon.com/fire-tv-stick-lite-latest-alexa-voice-remote-lite/dp/B091G4YP57/ref=s9_acsd_al_ot_c2_x_2_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=2c28288b-c275-4b38-8798-84c6244351a2&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$19.99",
+    cprice: "$29.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/51TjJOTfslL.AC_SX500.jpg",
+    url: "https://www.amazon.com/fire-tv-stick-with-3rd-gen-alexa-voice-remote/dp/B08C1W5N87/ref=s9_acsd_al_ot_c2_x_3_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=2c28288b-c275-4b38-8798-84c6244351a2&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$24.99",
+    cprice: "$39.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61-Ps9NldbL.AC_SX500.jpg",
+    url: "https://www.amazon.com/All-new-fire-tv-cube-4k-streaming-device/dp/B09BZZ3MM7/ref=s9_acsd_al_ot_c2_x_4_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=2c28288b-c275-4b38-8798-84c6244351a2&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$114.99",
+    cprice: "$139.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/418UoVylqyL.AC_SX250.jpg",
+    url: "https://www.amazon.com/introducing-amazon-fire-tv-32-inch-2-series-hd-smart-tv/dp/B09N6F9NV3/ref=s9_acsd_al_ot_c2_x_0_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=033799bd-396f-49e7-b551-d489f29af33d&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$119.99",
+    cprice: "$139.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71Nma1KADeL.AC_SX500.jpg",
+    url: "https://www.amazon.com/introducing-amazon-fire-tv-40-inch-2-series-hd-smart-tv/dp/B09N719G17/ref=s9_acsd_al_ot_c2_x_1_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=033799bd-396f-49e7-b551-d489f29af33d&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$178.99",
+    cprice: "$249.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/716flIFu1QL.AC_SX500.jpg",
+    url: "https://www.amazon.com/amazon-fire-tv-43-inch-4-series-4k-smart-tv/dp/B0B3HG269B/ref=s9_acsd_al_ot_c2_x_2_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=033799bd-396f-49e7-b551-d489f29af33d&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$249.99",
+    cprice: "369.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/716flIFu1QL.AC_SX500.jpg",
+    url: "https://www.amazon.com/amazon-fire-tv-50-inch-4-series-4k-smart-tv/dp/B0B3GTSQ9Q/ref=s9_acsd_al_ot_c2_x_3_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=033799bd-396f-49e7-b551-d489f29af33d&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$329.99",
+    cprice: "$449.99",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/716flIFu1QL.AC_SX500.jpg",
+    url: "https://www.amazon.com/amazon-fire-tv-55-inch-4-series-4k-smart-tv/dp/B0B3H6JPYZ/ref=s9_acsd_al_ot_c2_x_4_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=ECPXMEFAN2B16Y4T2DRG&pf_rd_p=033799bd-396f-49e7-b551-d489f29af33d&pf_rd_t=&pf_rd_i=8521791011",
+    tittle:
+      "Amazon Fire TV Stick Lite, free and live TV, Alexa Voice Remote Lite",
+    sprice: "$369.99",
+    cprice: "$519.99",
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        gap: "15px",
+        flexWrap: "wrap",
+      }}
+    >
+      {card.map((e) => {
+        return (
+          <div
+            style={{
+              width: "250px",
+              padding: "10px 20px",
+            }}
+          >
+            <img
+              style={{
+                width: "250px",
+                height: "150px,",
+              }}
+              src={e["img"]}
+              alt="img"
+            />
+            <a href={e.url}>{e.tittle}</a>
+
+            <h2>{e.sprice}</h2>
+            <del>{e.cprice}</del>
+
+            <button
+              style={{
+                backgroundColor: "yellow",
+                borderRadius: "30px",
+                border: "none",
+                padding: "10px 20px",
+              }}
+            >
+              Add to cart
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
